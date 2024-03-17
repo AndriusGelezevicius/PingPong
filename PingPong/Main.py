b18b8a1b26908428963c3f38b8fd202ed11d75a5
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-03-17 17:39:50 +0200

    Initial commit

---
+++ PingPong/Main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+WIDTH = 597
+HEIGHT = 350
+
+window = Tk()
+window.geometry("600x400")
+window.title("Ping Pong")
+
+canvas = Canvas(window, height=HEIGHT, width=WIDTH, background="black")
+
+
+window.mainloop()
