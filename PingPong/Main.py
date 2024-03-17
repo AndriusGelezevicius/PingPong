ea32bc97fb58586b1a40b25b83dbe03b25cbe878
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-03-17 17:50:12 +0200

    added player functionality

--- PingPong/Main.py
+++ PingPong/Main.py
@@ -2,6 +2,20 @@ from tkinter import *
 WIDTH = 597
 HEIGHT = 350
 
+
+def player1_move_up(event):
+    canvas.move(player1, 0, -10)
+
+def player1_move_down(event):
+    canvas.move(player1, 0, 10)
+
+def player2_move_up(event):
+    canvas.move(player2, 0, -10)
+
+def player2_move_down(event):
+    canvas.move(player2, 0, 10)
+
+
 window = Tk()
 window.geometry("600x400")
 window.title("Ping Pong")
@@ -12,4 +26,8 @@ player2 = canvas.create_rectangle(577,120,587,200, fill="white")
 ball = canvas.create_oval(287,140,297,150, fill="white")
 canvas.place(x=0, y=50)
 
+window.bind("<w>", player1_move_up)
+window.bind("<s>", player1_move_down)
+window.bind("<Up>", player2_move_up)
+window.bind("<Down>", player2_move_down)
 window.mainloop()
