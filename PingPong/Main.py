ba8de9c6aa495fff04770c8136f65b2e9f532169
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-03-17 18:09:05 +0200

    added player's collision with a ball

--- PingPong/Main.py
+++ PingPong/Main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 WIDTH = 597
 HEIGHT = 350
-
+xSpeed = 1 #speed of ball
+ySpeed = 1
 
 def player1_move_up(event):
     canvas.move(player1, 0, -10)
@@ -15,7 +16,36 @@ def player2_move_up(event):
 def player2_move_down(event):
     canvas.move(player2, 0, 10)
 
+def move_ball():
+    global xSpeed
+    global ySpeed
+    coordinates = canvas.coords(ball)
+    ball_width = coordinates[2] - coordinates[0]
+    ball_height = coordinates[3] - coordinates[1]
+    print(coordinates)
 
+    player2_coords = canvas.coords(player2)
+    if (coordinates[0] <= player2_coords[2] and
+            coordinates[1] <= player2_coords[3] and
+            coordinates[2] >= player2_coords[0] and
+            coordinates[3] >= player2_coords[1]):
+        xSpeed = -xSpeed  # Change ball's direction
+    player1_coords = canvas.coords(player1)
+    if (coordinates[0] <= player1_coords[2] and
+            coordinates[1] <= player1_coords[3] and
+            coordinates[2] >= player1_coords[0] and
+            coordinates[3] >= player1_coords[1]):
+        xSpeed = -xSpeed  # Change ball's direction
+
+    #if(coordinates[0]>=(WIDTH - ball_width) or coordinates[0]<0):
+     #   xSpeed = -xSpeed
+
+    if(coordinates[1]>=(HEIGHT - ball_width) or coordinates[1]<0):
+        ySpeed = -ySpeed
+
+    canvas.move(ball, xSpeed, 0)
+    canvas.move(ball, 0, ySpeed)
+    window.after(10, move_ball)
 window = Tk()
 window.geometry("600x400")
 window.title("Ping Pong")
@@ -30,4 +60,7 @@ window.bind("<w>", player1_move_up)
 window.bind("<s>", player1_move_down)
 window.bind("<Up>", player2_move_up)
 window.bind("<Down>", player2_move_down)
+
+move_ball()
+
 window.mainloop()
