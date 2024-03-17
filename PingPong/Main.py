b1f044b1b38d8b8fa9c05a04245c479a718caa55
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-03-17 17:45:32 +0200

    added players and ball

--- PingPong/Main.py
+++ PingPong/Main.py
@@ -7,6 +7,9 @@ window.geometry("600x400")
 window.title("Ping Pong")
 
 canvas = Canvas(window, height=HEIGHT, width=WIDTH, background="black")
-
+player1 = canvas.create_rectangle(10,120,20,200, fill="white")
+player2 = canvas.create_rectangle(577,120,587,200, fill="white")
+ball = canvas.create_oval(287,140,297,150, fill="white")
+canvas.place(x=0, y=50)
 
 window.mainloop()
